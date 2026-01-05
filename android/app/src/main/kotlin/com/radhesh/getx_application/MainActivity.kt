2cc21edf9143a9c6fd6fe39a2d53b71b0de05927
Radhesh-Roy <radheshroy0011@gmail.com>
2026-01-05 16:00:44 +0600

    counter

---
+++ android/app/src/main/kotlin/com/radhesh/getx_application/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.radhesh.getx_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/view/counter_ui/counter.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_application/controller/counter_controller/counter_controller.dart';
+
+class CounterGetxView extends StatelessWidget {
+   CounterGetxView({super.key});
+final CounterController counterController= Get.put(CounterController());
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.grey.shade100,
+        title: Text("Clickable Counter"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Obx(()=>Text("${counterController.counter}"),),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(onPressed: () {
+                  counterController.decrement();
+
+                }, child: Text("prev")),
+                ElevatedButton(onPressed: () {
+                  counterController.increment();
+
+                }, child: Text("Next")),
+              ],
+            )
+          ],
+        ),
+      ),
+
+    );
+  }
+}
